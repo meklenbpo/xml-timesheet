5b497ee99fcd1e36119122d8d7436f18d90f2b7a
meklenbpo <yk@kantynent.com>
2020-10-13 10:42:58 +0300

    Added tests for random_name, random_record

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,22 +9,23 @@ import random
 
 # Init. parameters
 
-def random_dt(min_dt: datetime, max_dt: datetime) -> str:
-    """Generate a random date/time object between two date/time 
+def random_dt(min_dt: datetime, max_dt: datetime) -> datetime:
+    """Generate a random date/time object between two provided date/time
     objects."""
     return '2020-01-01'
 
-def random_record() -> tuple:
-    """Generate a random timesheet record in predefined format, based on
-    predefined initialization values."""
+def random_name() -> datetime:
+    """Generate a random person name."""
     initials = 'abdefgiklmnoprstv'
     last_names = ['alekseyev', 'bobrova', 'demyanenko', 'evstigneeva',
         'fedorov', 'germanova', 'ivanov', 'klimova', 'losev', 'maksimova',
         'nikolaev', 'osina', 'petrov', 'razina', 'stepanov', 'tolstaya',
         'viktorov']
-    earliest_date = '2010-01-01'
-    latest_date = '2019-12-31'
-    pass
+    return f'{random.choice(initials)}.{random.choice(last_names)}'
+
+def random_record() -> str:
+    """Generate a random timesheet record in predefined format."""
+    return ''
 
 @pytest.fixture
 def sample_data_xml():
--- tests/test_unit_conftest.py
+++ tests/test_unit_conftest.py
@@ -4,10 +4,11 @@ A test sub-suite to test XML generating fixture functionality.
 
 import datetime as dt
 import pytest
+import xml.etree.ElementTree as ET
 
 import conftest
 
-
+# conftest.random_dt tests
 def test_random_dt_attribute_types_1():
     """Test that random_dt parameter 1 must be a datetime object, not
     string."""
@@ -36,4 +37,32 @@ def test_random_dt_output_less_than():
     rd = conftest.random_dt(dt.datetime(2012, 2, 1), dt.datetime(2014, 7, 5))
     assert rd <= dt.datetime(2014, 7, 5, 23, 59, 59)
 
+#conftest.random_name
+def test_random_name_type():
+    """Test for type of random_name output."""
+    name = conftest.random_name()
+    assert isinstance(name, str)
 
+#conftest.random_record
+def test_random_record_output_type():
+    """Test if random_record outputs a string."""
+    rec = conftest.random_record()
+    assert isinstance(rec, str)
+
+def test_random_record_valid_xml():
+    """Test if random_record output produces valid XML"""
+    rec = conftest.random_record()
+    xml = ET.fromstring(rec)
+
+def test_random_record_root():
+    """Test if random record root is a <person> tag."""
+    rec = conftest.random_record()
+    xml = ET.fromstring(rec)
+    assert xml.tag == 'person'
+
+@pytest.mark.parametrize("tag", ['start', 'end'])
+def test_random_record_has_tag(tag):
+    """Test if generated random record has the predefined XML tags."""
+    rec = conftest.random_record()
+    xml = ET.fromstring(rec)
+    assert xml.find(tag) is not None
