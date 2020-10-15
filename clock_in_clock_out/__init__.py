ade2d4e5d3ead2d558099a54642f82c2153ddb23
meklenbpo <yk@kantynent.com>
2020-10-15 21:12:07 +0300

    Updated documentation

--- clock_in_clock_out/__init__.py
+++ clock_in_clock_out/__init__.py
@@ -1,1 +1,30 @@
+"""
+Clock In Clock Out
+==================
+
+Clock-In-Clock-Out is a Python module for reading and analyzing time
+sheet data in a predefined XML format.
+
+It can:
+- Read time sheet data from an XML file of predefined format,
+- Filter time sheet data by start and end date,
+- Aggregate data by date and (optionally) person,
+- Report the results as a pandas DataFrame.
+
+Usage:
+
+>>> import clock_in_clock_out as cc
+>>> cc.query('sample.xml', start='01-01-2019', end='02-01-2019', names=False)
+date        time
+01-01-2019  12.1
+02-01-2019  4.8
+
+Breaking down by person:
+
+>>> cc.query('sample.xml', start='01-01-2019', end='01-01-2019', names=True)
+date        person      time
+01-01-2019  i.ivanov    6.1
+01-01-2019  s.tolstaya  6.0
+"""
+
 from .clock_in_clock_out import query
--- clock_in_clock_out/clock_in_clock_out.py
+++ clock_in_clock_out/clock_in_clock_out.py
@@ -1,18 +1,16 @@
 """
-Clock In Clock Out
-==================
+clock_in_clock_out
+------------------
 
-- Reads time sheet data from a formatted XML file (see data/time_data.xsd
-for XML schema),
-- Aggregates data by date and (optionally) person,
-- Reports aggregated results as a pandas DataFrame.
+includes main functionality for clock_in_clock_out module.
 """
 
 from datetime import datetime
 from lxml import etree
 import pandas as pd
 
-def schema() -> etree.XMLSchema:
+
+def _schema() -> etree.XMLSchema:
     """Prepare a predefined XML Schema and return it as etree.XMLSchema
     object."""
     s = b'''<?xml version="1.0" encoding="UTF-8" ?>
@@ -36,35 +34,35 @@ def schema() -> etree.XMLSchema:
     xml = etree.XML(s)
     return etree.XMLSchema(xml)
 
-def init_xml(xml_filename: str) -> etree.iterparse:
+def _init_xml(xml_filename: str) -> etree.iterparse:
     """Open and XML file with predefined settings and return it as an 
     etree.iterparse object."""
-    xml = etree.iterparse(xml_filename, tag='person', schema=schema(), 
+    xml = etree.iterparse(xml_filename, tag='person', schema=_schema(), 
                           events=('end',))
     return xml
 
-def extract_data_from_element(e: etree.Element) -> dict:
+def _extract_data_from_element(e: etree.Element) -> dict:
     """Extract useful data from XML Element and return it via a
     dictionary."""
     return {'full_name': e.get('full_name'),
             'start': e[0].text,
             'end': e[1].text}
 
-def clear_element(e: etree.Element):
+def _clear_element(e: etree.Element):
     """Clear XML Element and delete references to its parents."""
     e.clear()
     while e.getprevious() is not None:
         del e.getparent()[0]
 
-def get_batch(xml_filename: str, batch_size: int) -> tuple:
+def _get_batch(xml_filename: str, batch_size: int) -> tuple:
     """Read a `batch_size` of records from an XML file and yield it as
     a list of etree.Elements."""
     batch = []
     counter = 0
-    for _, element in init_xml(xml_filename):
+    for _, element in _init_xml(xml_filename):
         if counter < batch_size:
-            batch.append(extract_data_from_element(element))
-            clear_element(element)
+            batch.append(_extract_data_from_element(element))
+            _clear_element(element)
             counter += 1
         else:
             yield batch
@@ -72,7 +70,7 @@ def get_batch(xml_filename: str, batch_size: int) -> tuple:
             counter = 0
     yield batch
 
-def time_str_diff(dt_str_1: str, dt_str_2: str) -> float:
+def _time_str_diff(dt_str_1: str, dt_str_2: str) -> float:
     """Take two strings representing the date/time object in predefined
     format and calculate the time interval between them. Return the
     interval as a decimal number of hours."""
@@ -83,15 +81,15 @@ def time_str_diff(dt_str_1: str, dt_str_2: str) -> float:
     hours = round(delta.seconds / 60 / 60, 2)
     return hours
 
-def add_derivative_data(source_data: pd.DataFrame) -> pd.DataFrame:
+def _add_derivative_data(source_data: pd.DataFrame) -> pd.DataFrame:
     """Take a DataFrame of source data (full_name, start and end times)
     and calculate derivative values: `date` and `time`."""
     data = source_data.copy()
     data['date'] = data.start.str[:10]
-    data['time'] = data.apply(lambda x: time_str_diff(x.start, x.end), axis=1)
+    data['time'] = data.apply(lambda x: _time_str_diff(x.start, x.end), axis=1)
     return data
 
-def filter_data(dataset: pd.DataFrame, start_date: str, 
+def _filter_data(dataset: pd.DataFrame, start_date: str, 
                  end_date:str) -> pd.DataFrame:
     """Take a Dataframe of predefined structure and filter it by `date`
     column."""
@@ -105,7 +103,7 @@ def filter_data(dataset: pd.DataFrame, start_date: str,
                              (to_filter.date_dt <= end_dt)]
     return filtered[cols]
 
-def aggregate_data(dataset: pd.DataFrame, include_names: bool) -> pd.DataFrame:
+def _aggregate_data(dataset: pd.DataFrame, include_names: bool) -> pd.DataFrame:
     """Take a DataFrame of predefined structure and aggregate it on `date` and
     (optionally) `full_name` columns. Return as a pandas DataFrame."""
     to_agg = dataset.copy()
@@ -115,14 +113,14 @@ def aggregate_data(dataset: pd.DataFrame, include_names: bool) -> pd.DataFrame:
     else:
         return to_agg[['date', 'time']].groupby('date').sum().reset_index()
 
-def query(xml_filename: str, start_date: str = '01-01-1970',
-          end_date: str = '31-12-2199', incl_names: bool = False) -> pd.DataFrame:
+def query(xml_filename: str, start: str = '01-01-1970', end: str = '31-12-2199',
+          names: bool = False) -> pd.DataFrame:
     """Extract working time data from XML file chunk by chunk."""
     results = pd.DataFrame()
-    for batch in get_batch(xml_filename, 10):
+    for batch in _get_batch(xml_filename, 10):
         data_df = pd.DataFrame(batch)
-        augm_data = add_derivative_data(data_df)
-        filtered_data = filter_data(augm_data, start_date, end_date)
+        augm_data = _add_derivative_data(data_df)
+        filtered_data = _filter_data(augm_data, start, end)
         results = results.append(filtered_data)
-        results = aggregate_data(results, incl_names)
+        results = _aggregate_data(results, names)
     return results
